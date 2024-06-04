9e874b0b91fa8535bb89c7da8b5c79d461227ab8
PrematureDeer8 <yankeegabi.torres@gmail.com>
2024-06-03 19:40:07 -0700

    header outline algorithm

--- header_outline.c
+++ header_outline.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define trail 257
 
 typedef struct Point {
     int x;
@@ -6,18 +10,112 @@ typedef struct Point {
 } Point;
 
 
-float headerAlgorithm(int num_of_cols, int num_of_rows, int img[][num_of_cols], Point* start){
+float headerAlgorithm(int num_of_cols, int num_of_rows, int img[][num_of_cols], Point* start, float helper_line[]){
+    // helper_line -> int (start x), int (stop x), int (m), int (b)
+    int max_y = 0;
+    float dst_vect[2];
+    int divot_vect[2] = {0, 0};
+    int bias_factor = 2;
+    float obj_y;
+    float obj_x = helper_line[0];
+    float m = helper_line[2];
+    float b = helper_line[3];
     Point current;
     current.x = start->x;
     current.y = start->y;
-    // lets complete an L shape
-    // assuming we start from the top left corner 
-    // we have two moves down (positive direction) or right
 
-    // check to see if we can only move right or down (not either)
-    // if we cant move down then we must move right
-    if(!img[current.y + 1][current.x]){
+    // printf("X: %d\n", current.x);
+    // printf("Y: %d\n", current.y);
+    // printf("Xs: %.2f\n", helper_line[0]);
+    // printf("Xe: %.2f\n", helper_line[1]);
+    // printf("m: %.2f\n", helper_line[2]);
+    // printf("b: %.2f\n", helper_line[3]);
+    
+    while(true){
+        if(current.y > max_y){
+            printf("Max y: %d",max_y);
+            max_y = current.y;
+        }
+        // find the distance between the the helper line start point 
+        // and the starting point of the table
+        obj_y = obj_x * m + b;
+        dst_vect[0] = fabsf(obj_x - current.x);
+        dst_vect[1] = fabsf(obj_y - current.y);
+        // the x distance is greater than the y 
+        // try stepping to the right if the pixel is still black
+        if(dst_vect[0] > dst_vect[1] && img[current.y][current.x + 1] == 0){
+            // try to step in the x direction (if possible)
+            current.x++;
+            obj_x++;
+        }else if(dst_vect[1] > dst_vect[0] && img[current.y + 1][current.x] == 0)
+        {
+            current.y++;
+            obj_x++;
+        }else if(img[current.y][current.x + 1] == 0){
+            current.x++;
+
+        }else if(img[current.y + 1][current.x] == 0){
+            current.y++;
+        // both down and right are white
+        }else{
+            // we must move either up or left (or a combination of both)
+            // look at all the surrounding pixels and calculate their 
+            // gradient
+            // scale factor on the down and right is increased because of 
+            for(int i = 0; i < 8; i++){
+                switch(i){
+                    case 0: 
+                        divot_vect[1] -= (255 - img[current.y - 1][current.x]) / 255;
+                        break;
+                    case 1:
+                        divot_vect[0] += bias_factor * (255 - img[current.y - 1][current.x + 1]) / 255;
+                        divot_vect[1] -= (255 - img[current.y - 1][current.x + 1]) / 255;
+                        break;
+                    case 2:
+                        divot_vect[0] += bias_factor * (255 - img[current.y][current.x + 1]) / 255;
+                        break;
+                    case 3:
+                        divot_vect[0] += bias_factor * (255 - img[current.y + 1][current.x + 1]) / 255;
+                        divot_vect[1] += bias_factor * (255 - img[current.y + 1][current.x + 1]) / 255;
+                        break;
+                    case 4:
+                        divot_vect[1] += bias_factor * (255 - img[current.y + 1][current.x]) / 255;
+                        break;
+                    case 5:
+                        divot_vect[0] -= (255 - img[current.y + 1][current.x - 1]) / 255;
+                        divot_vect[1] += bias_factor * (255 - img[current.y + 1][current.x - 1]) / 255;
+                        break;
+                    case 6:
+                        divot_vect[0] -= (255 - img[current.y][current.x - 1]) / 255;
+                        break;
+                    case 7:
+                        divot_vect[0] -= (255 - img[current.y][current.x - 1]) / 255;
+                        divot_vect[1] -= (255 - img[current.y][current.x - 1]) / 255;
+                        break;
+                }
+            }
+            if(divot_vect[0] > 0){
+                current.x++;
+            }else{
+                current.x--;
+            }
+            if(divot_vect[1] > 0){
+                current.y++;
+            }else{
+                current.y--;
+            }
+
+        }
+        if(obj_x >= helper_line[1]){
+            obj_x = helper_line[1];
+            break;
+        }
+        // discourage going back from the tile that you came from
+        img[current.y][current.x] = trail;
+        printf("X: %d Y: %d\n",current.x, current.y);
         
+
     }
-    return 0;
+
+    return max_y;
 }
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pathlib
 from ctypes import *
 import cv2 as cv
 import numpy as np
+from invoice import Invoicer
 
 
 class Point(Structure):
@@ -9,10 +10,17 @@ class Point(Structure):
 
 def main():
     libObject = cdll.LoadLibrary("./header_outline.so");
-    img_path = pathlib.Path.home() / "Desktop" / "receipt-scanner" / "dst.jpg";
+    img_path = pathlib.Path.home() / "Desktop" / "receipt-scanner" / "scan.jpg";
+    invoice = Invoicer(str(img_path.absolute()));
+    invoice.table_outline();
+    invoice.align_table();
+    invoice.readText();
+    invoice.getHeaders();
+
     # grayscale and threshold the image
-    grayscale = cv.imread(str(img_path.absolute()), cv.IMREAD_GRAYSCALE);
-    ret, thresh = cv.threshold(grayscale, 127, 255, 0);
+    grayscale = cv.cvtColor(invoice.table_only, cv.COLOR_BGR2GRAY);
+    ret, thresh = cv.threshold(grayscale, 180, 255, 0);
+    cv.imwrite("thresh.jpg", thresh);
     # get starting point
     start_pt = np.unravel_index(np.argmin(thresh, axis=None), thresh.shape);
     c_start_pt = Point(*start_pt);
@@ -20,10 +28,17 @@ def main():
     # flatten image before passing into C function
     flat = thresh.flatten();
     # make an array compatible with C
-    float_arr = c_int * len(flat);
-    data = float_arr(*flat);
+    int_arr = c_int * len(flat);
+    data = int_arr(*flat);
+    # make line_helper array
+    float_arr = c_float * 4;
+    # xs, xe, m, b
+    line_helper = float_arr(invoice.start_x, invoice.end_x, *invoice.helper_line)
+    # draw best line of fit against a small amount of candidate headers
+    # have the algorithm try to follow the path without deviating to far from
+    # the line of best fit
     # pass data into C function
-    libObject.headerAlgorithm(data, c_int(cols), c_int(rows), pointer(c_start_pt));
+    print(libObject.headerAlgorithm( c_int(cols), c_int(rows), data,  pointer(c_start_pt),line_helper ));
 
 if( __name__ == "__main__"):
     main();
